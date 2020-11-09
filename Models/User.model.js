2aaf193829cf24d8d2558ad3cbd68f76315a84b4
ZhitingLu <luzhiting@outlook.com>
2020-11-09 22:27:39 +0100

    Added Password Hassing using bcrypt

--- Models/User.model.js
+++ Models/User.model.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const UserSchema = new Schema({
     email: {
@@ -14,6 +16,29 @@ const UserSchema = new Schema({
         required: true,
     }
 });
+
+UserSchema.pre('save', async function(next) { // this middleware is called before saving a user
+    try {
+        // console.log('called before saving a user')
+        const salt = await bcrypt.genSalt(saltRounds);
+        console.log(this.email, this.password);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword; 
+        next();
+        
+    } catch(error) {
+        next(error);
+    }
+});
+
+UserSchema.post('save', async function(next) {
+    try {
+        console.log('Called after saving a user')
+    } catch(error) {
+        next(error);
+    }
+});
+
 
 const User = mongoose.model('user', UserSchema);
 
