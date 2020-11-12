5f892e04e6d113dcca52e5a59728a3d50b8b85d4
ZhitingLu <luzhiting@outlook.com>
2020-11-12 18:26:18 +0100

    Handling JWT Error

--- helpers/jwt.helper.js
+++ helpers/jwt.helper.js
@@ -15,7 +15,7 @@ module.exports = {
             };
             const secret = process.env.ACCESS_TOKEN_SECRET;
             const options = {
-                expiresIn: "1h",
+                expiresIn: "15s",
                 issuer: "amazing.com",
                 audience: userId,
             };
@@ -38,11 +38,17 @@ module.exports = {
     verifyAccessToken: (req, res, next) => {
         if (!req.headers['authorization']) return next(createError.Unauthorized());  // verifying if the authorization is present in the headers
         const authHeader = req.headers['authorization']; // header: bearer + access token 
-        const bearerToken = authHeader.split(' '); 
+        const bearerToken = authHeader.split(' ');
         const token = bearerToken[1]; //actual token
         JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
             if (err) {
-                return next(createError.Unauthorized());
+                // if (err === 'JsonWebTokenError') {
+                //    return next(createError.Unauthorized());
+                // } else {
+                //    return next(createError.Unauthorized(err.message))
+                // };
+                const message = err.name === 'JsonWebTokenError' ? "Unauthorized" : err.message;
+                return next(createError.Unauthorized(message));
             }
             req.payload = payload;
             next();
